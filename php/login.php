b3d244dc359f05ae95b58505849e4ec0c30b619c
thexjoker-desktop <mhmdkais20@gmail.com>
2022-01-15 12:20:43 +0200

    cleaned up a littile

--- php/login.php
+++ php/login.php
@@ -8,16 +8,8 @@
     $username = $_POST['user'];;
     $password = $_POST['pass'];
 
-    // $username = stripslashes($username);
-    // $password = stripslashes($password);
-    // $username = mysql_real_escape_string($username);
-    // $username = mysql_real_escape_string($password);
-
     $sql = "select Permission from users where UserName = '$username' and Password = '$password' ";
-    // $sql = "select * from users";
     $row = mysqli_query($conn, $sql);
-    // $result = mysqli_fetch_array($row);
-    // $result = mysqli_fetch_all($row);
     $result = mysqli_fetch_assoc($row);
     if(!$result)
         echo "";
