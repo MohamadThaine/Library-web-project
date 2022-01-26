175f8ca70a7de166527a5a511b2249a90fd4608a
thexjoker-desktop <mhmdkais20@gmail.com>
2022-01-26 19:14:21 +0200

    completed the pop-ups and fixed the design a littile

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,36 @@
-const AddBooks = document.getElementById("AddBooks");
-const model_con = document.getElementById("model_con");
-const Close = document.getElementById("Close");
+const add_con = document.getElementById("add_con");
+const remove_con = document.getElementById("remove_con");
+const add_staff = document.getElementById("add_staff");
+const remove_staff = document.getElementById("remove_staff");
 
-AddBooks.addEventListener('click', () => {
-    model_con.classList.add('show');
+document.getElementById("AddBooks").addEventListener('click', () => {
+    add_con.classList.add('show');
 });
 
-Close.addEventListener('click', () => {
-    model_con.classList.remove('show');
+document.getElementById("RemoveBooks").addEventListener('click', () => {
+    remove_con.classList.add('show');
+});
+
+document.getElementById("AddStaff").addEventListener('click', () => {
+    add_staff.classList.add('show');
+});
+
+document.getElementById("RemoveStaff").addEventListener('click', () => {
+    remove_staff.classList.add('show');
+});
+
+document.getElementById("Closeadd").addEventListener('click', () => {
+    add_con.classList.remove('show');
+});
+
+document.getElementById("Closeremove").addEventListener('click', () => {
+    remove_con.classList.remove('show');
+});
+
+document.getElementById("CloseAddStaff").addEventListener('click', () => {
+    add_staff.classList.remove('show');
+});
+
+document.getElementById("CloseRemoveStaff").addEventListener('click', () => {
+    remove_staff.classList.remove('show');
 });
