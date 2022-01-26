47ca43de945f2f9a65c9289a6c8f93b80da6d267
thexjoker-desktop <mhmdkais20@gmail.com>
2022-01-26 23:36:32 +0200

    cleaned up a littile and added a menu for categories linked it to db, we must learn to deal with the picture part now

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,12 @@ const remove_con = document.getElementById("remove_con");
 const add_staff = document.getElementById("add_staff");
 const remove_staff = document.getElementById("remove_staff");
 
+$( document ).ready(function() {
+    GetAllCategories();
+  });
+
+document.getElementById("AddBook").addEventListener('click', AddBookToDB);
+
 document.getElementById("AddBooks").addEventListener('click', () => {
     add_con.classList.add('show');
 });
@@ -21,6 +27,7 @@ document.getElementById("RemoveStaff").addEventListener('click', () => {
 
 document.getElementById("Closeadd").addEventListener('click', () => {
     add_con.classList.remove('show');
+    document.querySelector("form").reset()
 });
 
 document.getElementById("Closeremove").addEventListener('click', () => {
@@ -34,3 +41,63 @@ document.getElementById("CloseAddStaff").addEventListener('click', () => {
 document.getElementById("CloseRemoveStaff").addEventListener('click', () => {
     remove_staff.classList.remove('show');
 });
+
+
+function AddBookToDB(e){
+    e.preventDefault();
+    var Target = document.querySelector("#AddBook").name;
+    var BookName = document.querySelector("#Book_Name").value;
+    var PublisherName = document.querySelector("#Publisher_Name").value;
+    var CategoryID  = document.querySelector("#Categories").value;
+    var Edition = document.querySelector("#Edition").value;
+    var ReleaseDate = document.querySelector("#Release_Date").value;
+    var BookCover = document.querySelector("#Cover").value;
+    var parms = `?Target=${Target}&BookName=${BookName}&PublisherName=${PublisherName}&CategoryID=${CategoryID }&Edition=${Edition}&ReleaseDate=${ReleaseDate}&BookCover=${BookCover}`
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",`../php/admin.php/${parms}`);
+    xhr.onload =  () => {
+        if (xhr.status == 200) {
+            QueryResult(xhr.responseText.trim());
+        } else {
+            console.log("something went wrong");
+        }
+    };
+    xhr.send();
+   
+    return false;  
+}
+
+function QueryResult(res){
+    if(Boolean(parseInt(res)))
+        console.log("success!");
+    else
+        console.log("failed :(!");
+}
+
+var jsoon;
+function GetAllCategories() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",`../php/AvailableCategories.php`);
+    // xhr.responseType = 'json';
+    xhr.onload =  () => {
+        if (xhr.status == 200) {
+            OptionsMaker(JSON.parse(xhr.response));
+        } else {
+            console.log("something went wrong");
+        }
+    };
+    
+    xhr.send();
+}
+
+
+function OptionsMaker(CategoriesJson){
+    CategoriesJson.forEach(category => {
+        $('#Categories').append($('<option>', {
+            value: category[0],
+            text: category[1]
+        }));
+    });
+    
+    
+}
